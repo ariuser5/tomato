28fa5baaea24e65f544dde11a419ad494c7a72ee
Darius Emanuel Ciuverca <dariusciuverca@gmail.com>
2026-03-19 00:48:13 +0200

    Remove unused Patterns.psm1 module and its dependencies

--- base/tomatoflow/organization/Patterns.psm1
+++
@@ -1,7 +0,0 @@
-Set-StrictMode -Version Latest
-$ErrorActionPreference = 'Stop'
-
-$monthUtilsModule = Join-Path $PSScriptRoot '.\modules\MonthUtils.psm1'
-Import-Module $monthUtilsModule -Force
-
-Export-ModuleMember -Variable MonthNames, MonthPattern
